3ce5d9134b7e5a2692ca801d17d9d44a7aae2d37
KateBin™ <72105928+Katebin@users.noreply.github.com>
2023-01-19 10:52:27 -0500

    [feat] Initial Commit
    Includes basic code for starting

---
+++ src/Main/Main.ino
@@ -0,0 +1,11 @@
+#include <PULSE.h>
+PULSE pulse; // iniate the robot object
+
+void setup() {
+  // put your setup code here, to run once:
+    pulse.PulseBegin(); // start tetrix
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
